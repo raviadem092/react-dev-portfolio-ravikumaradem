96fedb156f6d68aaa4349c056950f92c3ddf23f9
A Ravi Kumar <raviadem8055@gmail.com>
2026-07-01 22:15:33 +0530

    added standard environment variable.

--- src/components/chatbot/chat.service.ts
+++ src/components/chatbot/chat.service.ts
@@ -2,7 +2,7 @@ import {
     ChatResponse,
     SuggestedQuestionsResponse
 } from "./chat.types";
-const API_BASE_URL = process.env.ASK_RAVI_CHAT_BOT_API_BASE_URL;
+const API_BASE_URL = process.env.REACT_APP_ASK_RAVI_CHAT_BOT_API_BASE_URL;
 const ENDPOINTS = {
     CHAT: "/api/chat",
     QUESTIONS: "/api/questions",
